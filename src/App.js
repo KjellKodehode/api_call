dc79ee6cbfb1d6081a587a1c27f226e654db8e36
KjellKodeHode <kjell.arne.kodehode@gmail.com>
2022-05-13 10:43:17 +0200

    new way test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-// import React, { useState, useEffect } from "react";
-// import Joke from "./components/Joke.jsx";
 import JokeApp from "./components/GetJoke.jsx";
 import "./assets/css/fonts.css";
 
@@ -9,7 +5,6 @@ function App() {
   return (
     <div className="container">
       <div>
-        {/* <Joke /> */}
         <JokeApp />
       </div>
     </div>
